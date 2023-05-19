83c7df481834d5e5703224877fbac189a771a553
Denis <vacek2802@gmail.com>
2023-05-19 09:54:14 +0300

    update register request

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -7,7 +7,6 @@ const slug = require('slug')
 const router = createRouter<NextApiRequest, NextApiResponse>();
 router
   .post(async (req, res) => {
-    // TODO send req.body
     const user = await container.resolve("UserService").registerUser(req.body)
     res.status(200).json(user)
   })
