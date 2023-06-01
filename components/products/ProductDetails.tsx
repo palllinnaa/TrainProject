82fb6ce132f5d55870d275cc01dcfd1e2a0b05a6
Denis <vacek2802@gmail.com>
2023-06-01 16:10:17 +0300

    update product's components

--- components/products/ProductDetails.tsx
+++ components/products/ProductDetails.tsx
@@ -9,17 +9,17 @@ export default function ProductDetails(props) {
             <div className="">
                 <div className="">
                     <div className='font-serif '>
-                        <img className='object-cover h-full mt-3 rounded-lg shadow-md md:h-80 md:w-full lg:w-auto' src={product.image} alt="meals/drinks image" />
+                        <img className='object-cover h-full mt-3 rounded-lg shadow-md md:h-80 md:w-full lg:w-auto' src={product?.image} alt="meals/drinks image" />
                     </div>
                     <div className="px-2 mt-2">
-                        <PropertyBox properties={product.property} />
+                        <PropertyBox properties={product?.property} />
                     </div>
                 </div>
                 <div className="">
-                    <h1 className='mt-3 text-lg font-semibold text-gray-900'>{product.productName}</h1>
-                    <span className="mt-4 text-sm text-gray-500 text-clip ">{product.description}</span>
+                    <h1 className='mt-3 text-lg font-semibold text-gray-900'>{product?.productName}</h1>
+                    <span className="mt-4 text-sm text-gray-500 text-clip ">{product?.description}</span>
                     {/* <div className="md:absolute md:bottom-8">
-                        <Review reviewCount={product.reviewCount} rating={product.rating}></Review>
+                        <Review reviewCount={product?.reviewCount} rating={product?.rating}></Review>
                     </div> */}
                 </div>
             </div>
@@ -27,17 +27,17 @@ export default function ProductDetails(props) {
                 <h3 className='mt-2 font-semibold text-gray-900 text-md '>Ingredients:</h3>
                 <div>
                     {
-                        product.ingredients?.map((ingredient) => (
+                        product?.ingredients?.map((ingredient) => (
                             <p className='mt-1 ml-3 text-sm text-gray-600'>&#9702; {ingredient}</p>
                         ))
                     }
                 </div>
             </div>
-            <Review reviewCount={product.reviewCount} rating={product.rating}></Review>
+            <Review reviewCount={product?.reviewCount} rating={product?.rating}></Review>
             <div className="flex items-center justify-between my-3 ">
                 <div className="">
                     <span className='font-semibold text-gray-600 text-md'>Price: </span>
-                    <span className='text-lg font-semibold text-gray-600'>${product.price}</span>
+                    <span className='text-lg font-semibold text-gray-600'>${product?.price}</span>
                     <span className='ml-1 text-sm text-gray-400'>/one</span>
                 </div>
                 <div className="flex items-center">
