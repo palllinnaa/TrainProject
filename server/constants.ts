fb2ef7d90ed923c93523b89954c5bdde6e9ae071
Denis <vacek2802@gmail.com>
2023-06-02 13:56:31 +0300

    create fetch wrapper

--- server/constants.ts
+++ server/constants.ts
@@ -1,5 +1,14 @@
+import Entity from "../src/models/entity";
+
 export enum Roles {
-    ADMIN = "admin",
-    SELLER = "seller",
-    USER = "user"
-  }
+  ADMIN = "admin",
+  SELLER = "seller",
+  USER = "user"
+}
+
+export enum METHODS {
+  GET = "GET",
+  POST = "POST"
+}
+
+export const entity = new Entity();
