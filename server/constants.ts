111320352225b851f10df9e38b280aa12480f41d
Denis <vacek2802@gmail.com>
2023-06-26 12:19:55 +0300

    delete unused constants

--- server/constants.ts
+++ server/constants.ts
@@ -1,9 +1,3 @@
-import ProductSaga from "../redux/models/product";
-import ReviewSaga from "../redux/models/review";
-import StoreSaga from "../redux/models/store";
-import UserSaga from "../redux/models/user";
-import AuthSaga from '../redux/models/auth';
-
 export enum Roles {
   ADMIN = "admin",
   SELLER = "seller",
@@ -14,11 +8,5 @@ export enum METHODS {
   GET = "GET",
   POST = "POST"
 }
-
-export const userSaga = new UserSaga;
-export const authSaga = new AuthSaga;
-export const storeSaga = new StoreSaga;
-export const reviewSaga = new ReviewSaga;
-export const productSaga = new ProductSaga;
 
 export const isEmpty = (val: any) => val == null || !(Object.keys(val) || val).length;
