87e8a05d75c39c21dd4cdd4b417309eb8183135a
Denis <vacek2802@gmail.com>
2023-07-17 10:25:46 +0300

    change store

--- redux/store.ts
+++ redux/store.ts
@@ -1,11 +1,12 @@
 import { IState } from './../server/interfaces/common';
-import { applyMiddleware, compose, Dispatch, Store } from "redux";
+import { AnyAction, applyMiddleware, compose, Dispatch } from "redux";
 import BaseClientContext from "./baseClientContext";
 import { all } from 'redux-saga/effects';
 import Entity from './models/entity';
-import createSagaMiddleware from 'redux-saga'
-import { configureStore } from '@reduxjs/toolkit'
+import createSagaMiddleware, { Task } from 'redux-saga'
+import { configureStore, EnhancedStore } from '@reduxjs/toolkit'
 import rootReducer from './reducers';
+import { createWrapper } from 'next-redux-wrapper';
 
 declare global {
   interface Window {
@@ -13,9 +14,15 @@ declare global {
   }
 }
 
+export interface SagaStore extends EnhancedStore<any, AnyAction> {
+  sagaTask?: Task;
+}
+
 export default class ReduxStore extends BaseClientContext {
-  private _store: Store;
-  public get store(): Store<IState> {
+  private _store: EnhancedStore<any, AnyAction>;
+  public _wrapper;
+
+  public get store(): EnhancedStore<any, AnyAction> {
     return this._store;
   }
 
@@ -34,10 +41,10 @@ export default class ReduxStore extends BaseClientContext {
       process.env.NODE_ENV === 'development' ||
       process.env.DEBUG_PROD === 'true';
     if (!isDebug) {
-      const { store } = this.configureProdStore();
+      const store = this.configureProdStore();
       this._store = store;
     } else {
-      const { store } = this.configureDevStore();
+      const store = this.configureDevStore();
       this._store = store;
     }
   }
@@ -48,39 +55,50 @@ export default class ReduxStore extends BaseClientContext {
   });
 
   private configureProdStore(initialState?: IState) {
-    const sagaMiddleware = createSagaMiddleware();
+    const makeStore = () => {
+      const sagaMiddleware = createSagaMiddleware();
 
-    const store: Store<IState> = configureStore({
-      reducer: rootReducer,
-      preloadedState: initialState,
-      middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(sagaMiddleware)
-    });
+      const store: EnhancedStore<any, AnyAction> = configureStore({
+        reducer: rootReducer,
+        preloadedState: initialState,
+        middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(sagaMiddleware)
+      });
 
-    sagaMiddleware.run(this.rootSaga);
-    return { store };
+      (store as SagaStore).sagaTask = sagaMiddleware.run(this.rootSaga);
+      return store;
+    }
+
+    this._wrapper = createWrapper<EnhancedStore<any, AnyAction>>(makeStore, { debug: false });
+    return this._wrapper;
   }
 
-  private configureDevStore = (initialState?: IState) => {
-    const middleware = [];
-    const enhancers = [];
+  private configureDevStore(initialState?: IState) {
+    const makeStore = () => {
 
-    const sagaMiddleware = createSagaMiddleware();
-    middleware.push(sagaMiddleware);
+      const middleware = [];
+      const enhancers = [];
 
-    const composeEnhancers = (typeof window == 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+      const sagaMiddleware = createSagaMiddleware();
+      middleware.push(sagaMiddleware);
 
-    enhancers.push(applyMiddleware(...middleware));
-    const enhancer = composeEnhancers(...enhancers);
+      const composeEnhancers = (typeof window == 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
-    const store: Store<IState> = configureStore({
-      reducer: rootReducer,
-      preloadedState: initialState,
-      middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(sagaMiddleware),   // i added
-      //TODO add enhancers
-      // enhancers: enhancer
-    });
+      enhancers.push(applyMiddleware(...middleware));
+      const enhancer = composeEnhancers(...enhancers);
 
-    sagaMiddleware.run(this.rootSaga);
-    return { store };
+      const store: EnhancedStore<any, AnyAction> = configureStore({
+        reducer: rootReducer,
+        preloadedState: initialState,
+        middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(sagaMiddleware),
+        //TODO add enhancers
+        // enhancers: enhancer
+      });
+
+      (store as SagaStore).sagaTask = sagaMiddleware.run(this.rootSaga);
+      return store;
+    }
+
+    this._wrapper = createWrapper<EnhancedStore<any, AnyAction>>(makeStore, { debug: true });
+    return this._wrapper;
   }
 }
