3b06e7735f1f4dac05991802631bef173ce10063
Denis <vacek2802@gmail.com>
2023-07-17 10:27:18 +0300

    add hydrate and change reducers

--- redux/reducers/authReducer.ts
+++ redux/reducers/authReducer.ts
@@ -1,30 +1,28 @@
 import { isEmpty } from '../../server/constants';
 import { IStateAuthData } from '../../server/interfaces/common';
 
-const initialIdentity = {
-    identity: null,
-    error: null
-}
-
-export default function authReducer(state: IStateAuthData = initialIdentity, action: any) {
+export default function authReducer(state: IStateAuthData, action: any) {
+    const newState = { ...state };
     switch (action.type) {
         case 'GET_IDENTITY':
+            delete newState.error
             state = {
-                ...initialIdentity,
+                ...newState,
                 identity: action.payload
             }
             break;
         case 'GET_IDENTITY_ERROR':
+            delete newState.identity
             state = {
-                ...initialIdentity,
+                ...newState,
                 error: action.payload
             }
             break;
         case 'CLEAR_IDENTITY_ERROR':
             if ('error' in state && !isEmpty(state.error)) {
+                delete newState.error
                 state = {
-                    ...state,
-                    error: null,
+                    ...newState
                 }
             } break;
         default:
