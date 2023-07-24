1ca0ce61b9c435cbcbb898e9e472711e2ec909e7
Denis <vacek2802@gmail.com>
2023-07-24 12:25:19 +0300

    create search filter

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,10 +1,16 @@
-export default function Search() {
+export default function Search(props) {
+  const { handleChange } = props;
+
   return (
     <div className="relative flex-1">
       <div className="absolute inset-y-0 flex items-center pl-3 right-4">
         <img className="w-5 h-5" src="images/search_icon.png" alt="search icon" />
       </div>
-      <input className="w-full py-2 pl-10 pr-4 font-serif text-gray-800 bg-gray-100 border border-white rounded-full  focus:border-gray-200 focus:outline-none focus:bg-white focus:text-gray-900" placeholder="Search by name" />
+      <input
+        className="w-full py-2 pl-10 pr-4 font-serif text-gray-800 bg-gray-100 border border-white rounded-full focus:border-gray-200 focus:outline-none focus:bg-white focus:text-gray-900"
+        placeholder="Search by name"
+        onChange={handleChange}
+      />
     </div>
   )
 }
--- components/products/index.tsx
+++ components/products/index.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+import { isEmpty } from '../../server/constants';
 import { IProduct } from '../../server/interfaces/common';
 import ProductCard from './ProductCard';
 interface IPropertyCardProps {
@@ -9,7 +11,7 @@ export default function ProductBox(props: IPropertyCardProps) {
     const { data } = props;
 
     return (
-        <div className="grid px-4 sm:grid-cols-2 lg:grid-cols-4 sm:pb-8 sm:justify-between">
+        !isEmpty(data) ? <div className="grid px-4 sm:grid-cols-2 lg:grid-cols-4 sm:pb-8 sm:justify-between" >
             {
                 Object.values(data)?.map((product, id) => (
                     <div key={id}>
@@ -17,6 +19,8 @@ export default function ProductBox(props: IPropertyCardProps) {
                     </div>
                 ))
             }
+        </div > : <div className='flex justify-center w-full py-4 mb-6 border-gray-200 border-y'>
+            <p className='text-red-500 '>Can't find product with that name!</p>
         </div>
     );
 }
