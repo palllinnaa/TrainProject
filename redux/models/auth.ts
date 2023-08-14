074886fd747bf93c074699b6dbb1259b84e0c172
Denis <vacek2802@gmail.com>
2023-08-14 10:34:12 +0300

    update actions

--- redux/models/auth.ts
+++ redux/models/auth.ts
@@ -1,7 +1,7 @@
 import { METHODS } from './../../server/constants';
 import Entity from "./entity"
 import { call, put } from 'redux-saga/effects'
-import { getIdentity, getIdentityError } from "../actions/auth";
+import { clearIdentityMessage, getIdentity, getIdentityError, identityMessage } from "../actions/auth";
 import Router from 'next/router'
 import action from '../decorators/action';
 
@@ -15,9 +15,11 @@ export default class AuthSaga extends Entity {
     protected * fetchLoginUser(data) {
         try {
             const result = yield call(this.fetchWrapper, 'login', METHODS.POST, data.payload);
-            if (result.identity) {
-                yield put(getIdentity(result.identity));
-                Router.push(`/user/${result.identity.id}`);
+            if (result.data) {
+                yield put(getIdentity(result.data));
+                const { message, messageType } = result;
+                yield put(identityMessage({ message: { message, messageType } }));
+                Router.push(`/user/${result.data.id}`);
             }
         } catch (error) {
             yield put(getIdentityError(error.message));
@@ -28,9 +30,11 @@ export default class AuthSaga extends Entity {
     protected * fetchRegisterUser(data) {
         try {
             const result = yield call(this.fetchWrapper, 'register', METHODS.POST, data.payload);
-            if (result.identity) {
-                yield put(getIdentity(result.identity));
-                Router.push(`/user/${result.identity.id}`);
+            if (result.data) {
+                yield put(getIdentity(result.data));
+                const { message, messageType } = result;
+                yield put(identityMessage({ message: { message, messageType } }));
+                Router.push(`/user/${result.data.id}`);
             }
         } catch (error) {
             yield put(getIdentityError(error.message));
